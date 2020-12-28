28e10f20d39f3eb7bbabf03809e8e2a51184a5c3
Steve Marquez <steveantonio00@gmail.com>
2020-12-28 14:36:36 -0800

    Implement products component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Banner from './components/Banner';
+import Products from './components/Products';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyles';
+import { productData } from './components/Products/data';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Banner />
+      <Products heading={'Choose your favorite'} data={productData} />
     </Router>
   );
 }
---
+++ src/components/Products/data.js
@@ -0,0 +1,33 @@
+import product1 from '../../images/pizza-1.jpg';
+import product2 from '../../images/pizza-2.jpg';
+import product3 from '../../images/pizza-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatos, olives and pesto',
+    price: '$10.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Hawaiian Pizza',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatos, olives and pesto',
+    price: '$15.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'SF Pizza',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatos, olives and pesto',
+    price: '$19.99',
+    button: 'Add to Cart',
+  },
+];
